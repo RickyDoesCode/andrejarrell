83c6f8a0c477e03768f4c91e231335160982edcb
Andre Jarrell <mail@andrejarrell.com>
2020-04-12 14:45:02 +1000

    added chat

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,29 +1,38 @@
 (function($) {
-    "use strict"; // Start of use strict
-  
-    // Smooth scrolling using jQuery easing
+    "use strict";
+
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top)
-          }, 1000, "easeInOutExpo");
-          return false;
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
         }
-      }
     });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
+
     $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
+        $('.navbar-collapse').collapse('hide');
     });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
+
     $('body').scrollspy({
-      target: '#sideNav'
+        target: '#sideNav'
     });
-  
-  })(jQuery); // End of use strict
-  
+
+})(jQuery);
+
+
+const gitalk = new Gitalk({
+    clientID: '3d203b790df8ffae149c',
+    clientSecret: '5978c49eb122c9d8b44574d4a4d7bc0b90e1de96',
+    repo: 'Website',
+    owner: 'andrejarrell',
+    admin: ['andrejarrell'],
+    id: 'gitalk',
+    distractionFreeMode: false
+});
+
+gitalk.render('gitalk-container')
