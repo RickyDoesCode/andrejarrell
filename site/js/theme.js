2af0f355877299fe129a0fcc381710fcf6990939
Andre Jarrell <mail@andrejarrell.com>
2020-04-14 03:52:39 +1000

    test toggle

--- site/js/theme.js
+++ site/js/theme.js
@@ -1,15 +1,19 @@
 let body  = document.getElementsByTagName('body')[0];
+let btn = document.getElementById('toggle');
 
 if (localStorage.theme === 'dark') {
     body.setAttribute('data-theme', 'dark');
+    btn.innerTEXT('🌙 Toggle Theme');
 };
 
 let toggle = () => {
     if (localStorage.theme === 'light') {
         body.setAttribute('data-theme', 'dark');
+        btn.innerTEXT('🌙 Toggle Theme');
         localStorage.theme = 'dark';
     } else {
         body.setAttribute('data-theme', 'light');
+        btn.innerTEXT('☀️ Toggle Theme');
         localStorage.theme = 'light';
     };
 };
